149e19bc43b78dd461c61eba2172f7a6e9986341
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2023-11-24 18:58:23 +0900

    Make DB conncetion pool

--- src/loader/db.js
+++ src/loader/db.js
@@ -1,15 +1,49 @@
 const mysql2 = require('mysql2');
-const dbConfig = require('../../config/db_config');
+const genericPool = require('generic-pool');
 
-const pool = mysql2.createPool(dbConfig);
+const { dbConfig } = require('../../config/db_config');
 
-exports.dbCon = function (query) {
-  pool.getConnection(async (err, conn) => {
-    conn.query(query);
-    if (!err) {
-      conn.release();
-    } else {
-      throw err;
-    }
-  });
+const pool = genericPool.createPool({
+  // 커넥션 만들기
+  create: function () {
+    return new Promise((resolve, reject) => {
+      const connection = mysql2.createConnection(dbConfig);
+      connection.connect((err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(connection);
+        }
+      });
+    });
+    // async function() {
+    //     try {
+    //         const connection = await mysql2.createConnection(dbConfig);
+    //         return connection;
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // }
+  },
+  // 커넥션 닫기
+  destroy: function (connection) {
+    return new Promise((resolve, reject) => [
+      connection.end((err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      }),
+    ]);
+  },
+});
+
+module.exports = {
+  getConnection: () => {
+    return pool.acquire();
+  },
+  releaseConnection: (connection) => {
+    return pool.release(connection);
+  },
 };
