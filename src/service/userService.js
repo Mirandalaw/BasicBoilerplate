18610c3de3cafb0547eef1a080a55c490a27b586
JeongPark <shrup5@naver.com>
2023-11-28 20:12:55 +0900

    Add module genericpool

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,14 +1,40 @@
-const dbCon = require('../loader/db');
+const db = require('../loader/db');
 
 module.exports = {
   findAll: async () => {
+    let connection;
     try {
-      let query = 'SELECT * FROM user';
-      const user = await connect(query);
-      console.log(user);
-      return user;
+      connection = await db.getConnection();
+      const query = 'SELECT * FROM user';
+      const results = await executeQuery(connection, query);
+      return results;
     } catch (err) {
-      console.error(err);
+      console.error('Error :', err.stack);
+    } finally {
+      if (connection) {
+        await db.releaseConnection(connection);
+      }
     }
   },
 };
+
+function executeQuery(connection, query) {
+  return new Promise((resolve, reject) => {
+    connection.query(query, (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+}
+
+async function executeQuery(connection, query) {
+  try {
+    const [results] = await connection.query(query);
+    return results;
+  } catch (err) {```
+    throw err;
+  }
+}
